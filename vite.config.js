4c934d90fd1ce5543f25f71dcebb5df2fd3cacda
ruijpedro <rjpedro@gmail.com>
2026-06-17 23:43:28 +0100

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
+  base: '/RJP_Obras/'
 })
